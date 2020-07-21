4c0d90d9e0af619278e61945d073f9db2e231315
hoailakqq <thuonghoai.financial@gmail.com>
2020-07-21 23:55:38 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
-  appID: '',
+  appID: '1:751429136168:web:f2756e26ddf1b4b2c6ea1d',
   config: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: "AIzaSyB86uStSae4vxIAO9DGfW6cDJTPuG3g7Kg",
+    authDomain: "webtrans-f506e.firebaseapp.com",
+    databaseURL: "https://webtrans-f506e.firebaseio.com",
+    projectId: "webtrans-f506e",
+    storageBucket: "webtrans-f506e.appspot.com",
+    messagingSenderId: "751429136168",
+    appId: "1:751429136168:web:f2756e26ddf1b4b2c6ea1d",
+    measurementId: "G-6CX1PE0404"
   }
 };
